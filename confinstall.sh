a8360a460c9c7fe9ca28fb66eeff7015b49b7bf2
Pine <77921296+PineTreePizza@users.noreply.github.com>
2022-11-18 22:29:45 +0300

    Update confinstall.sh

--- confinstall.sh
+++ confinstall.sh
@@ -17,9 +17,18 @@ sudo pacman -S qbittorrent
 sudo pacman -S steam
 sudo pacman -S discord
 sudo pacman -S wine-mono
+sudo pacman -S wine
+sudo pacman -S mono
+sudo pacman -S lmms
 sudo pacman -S zip
 sudo pacman -S unzip
 sudo chmod +s /bin/light
+
+#sublime text
+
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu sublime-text
 
 #git builds
 sudo pacman -S --needed base-devel git
@@ -66,6 +75,7 @@ cd $HOME
 #yay pkg
 
 yay -S dropbox
+yay -S github-desktop-git
 
 #end
 sudo pacman -Syu
