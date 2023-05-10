5590b2d11f43e50600e803bbb82264139bbfe9b4
Pine <77921296+PineTreePizza@users.noreply.github.com>
2023-05-10 15:08:59 +0300

    Update confinstall.sh

--- confinstall.sh
+++ confinstall.sh
@@ -38,6 +38,7 @@ sudo pacman -S vivaldi-ffmpeg-codecs
 sudo pacman -S vivaldi
 sudo pacman -S lightdm-gtk-greeter-settings
 sudo pacman -S shotcut
+sudo pacman -S vlc
 
 sudo chmod +s /bin/light
 
@@ -49,14 +50,15 @@ cd yay
 makepkg -si
 cd $HOME
 
-git clone https://github.com/PineTreePizza/Linuxi3Config.git
-cd Linuxi3Config
-./itch-install
-cd Fonts
-sudo mv pine-fonts /usr/share/fonts/ 
-cd $HOME/Linuxi3Config
+git clone https://github.com/PineTreePizza/AltConfig.git
+cd AltConfig
 
-mv Wallpapers $HOME/
+./itch-install
+
+sudo rm -R /usr/share/fonts
+sudo mv fonts /usr/share/
+
+cd $HOME/AltConfig
 
 rm -R $HOME/.config/i3
 mv i3 $HOME/.config/
@@ -67,28 +69,39 @@ mv picom.conf $HOME/.config/
 rm -R $HOME/.config/rofi
 mv rofi $HOME/.config/
 
-rm -R $HOME/.config/terminator
-mv terminator $HOME/.config/
+rm -R $HOME/.config/alacritty
+mv alacritty $HOME/.config/
+
+rm -R $HOME/.config/mc
+mv mc $HOME/.config/
+
+rm -R $HOME/.config/polybar
+mv polybar $HOME/.config/
+
+rm -R $HOME/.config/fish
+mv fish $HOME/.config/
+
+rm -R $HOME/.config/tmux
+mv tmux $HOME/.config/
+
+sudo mv imgs /
 
 cd $HOME
+cd AltConfig
 
-unzip gtk-master.zip
-unzip Dracula.zip
+sudo rm -R /usr/share/themes
+sudo mv themes /usr/share/
 
-sudo mkdir /usr/share/themes/
-sudo mv gtk-master /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme "Graphite-Dark"
+gsettings set org.gnome.desktop.wm.preferences theme "Graphite-Dark"
 
-gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
-gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+sudo rm -R /usr/share/icons
+sudo mv icons /usr/share/
 
-sudo mkdir /usr/share/icons/
-sudo mv Dracula /usr/share/icons/
+gsettings set org.gnome.desktop.interface icon-theme "Bibata-Modern-Classic"
 
-gsettings set org.gnome.desktop.interface icon-theme "Dracula"
-
-sudo mkdir $HOME/emacs.d
-sudo rm init.el $HOME/emacs.d
-sudo mv init.el $HOME/emacs.d
+sudo rm -R $HOME/.emacs.d
+sudo mv .emacs.d $HOME/
 
 cd $HOME
 
